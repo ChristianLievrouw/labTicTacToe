c544413ad48bd0a2ca2808577a17c420a4e2f475
ChristianLievrouw <celievrouw1@gmail.com>
2020-08-30 16:54:55 -0500

    game working

--- TicTacToe/Classes/Board.cs
+++ TicTacToe/Classes/Board.cs
@@ -1,27 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-namespace Lab04_TicTacToe.Classes
+
+namespace TicTacToe.Classes
 {
 	class Board
 	{
 		/// <summary>
 		/// Tic Tac Toe Gameboard states
 		/// </summary>
-		public static string[,] GameBoard = new string[,]
+		public string[,] GameBoard = new string[,]
 		{
 			{"1", "2", "3"},
 			{"4", "5", "6"},
 			{"7", "8", "9"},
 		};
-		public static void DisplayBoard()
+		public void DisplayBoard()
 		{
 			//TODO: Output the board to the console
 			for (int i = 0; i < 3; i++)
 			{
 				for (int j = 0; j < 3; j++)
 				{
-					Console.Write($"|{GameBoard[i, j]}|");
+					Console.Write($"|{GameBoard[i, j]}| ");
 				}
 				Console.Write("\n");
 			}
--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using Lab04_TicTacToe.Classes;
+using TicTacToe.Classes;
+
 
 namespace TicTacToe
 {
@@ -8,22 +9,50 @@ namespace TicTacToe
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Board.DisplayBoard();
-            //StartGame();
+            StartGame();
         }
 
-            static void StartGame()
-            {
-            // TODO: Setup your game. Create a new method that creates your players and instantiates the game class. Call that method in your Main method.
-            // You are requesting a Winner to be returned, Determine who the winner is output the celebratory message to the correct player. If it's a draw, tell them that there is no winner.
-            Console.Write("Player 1 enter your name: ");
-            string input1 = Console.ReadLine();
-            Console.Write("Player 2 enter your name: ");
-            string input2 = Console.ReadLine();
-            //Player p1 = new Player();
-            //Player p2 = new Player();
-            }
+        static void StartGame()
+        {
+            bool play = true;
 
-        
+            while (play == true)
+            {
+                // TODO: Setup your game. Create a new method that creates your players and instantiates the game class. Call that method in your Main method.
+                // You are requesting a Winner to be returned, Determine who the winner is output the celebratory message to the correct player. If it's a draw, tell them that there is no winner.
+                Console.Write("Player 1 enter your name: ");
+                string input1 = Console.ReadLine();
+                Console.Write("Player 2 enter your name: ");
+                string input2 = Console.ReadLine();
+                Player p1 = new Player();
+                Player p2 = new Player();
+                p1.Name = input1;
+                p2.Name = input2; ;
+                p1.Marker = "X";
+                p2.Marker = "O";
+                Game newGame = new Game(p1, p2);
+                
+                Player winner = newGame.Play();
+                if (winner.Name != "Draw")
+                {
+                    Console.WriteLine("Winner {0}!!!", winner.Name);
+                }
+                else
+                {
+                    Console.WriteLine("Draw");
+                }
+
+                Console.Write("Play again? Y/N");
+                string input = Console.ReadLine();
+                if (input == "Y" || input == "y" || input == "yes" || input == "yes")
+                {
+                    play = true;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
     }
 }
